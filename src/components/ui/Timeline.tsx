f046472d0cb96a57f599180a47197decbdb37fff
udbhav <udbhav@UDBHAV>
2025-10-23 20:42:57 -0700

    Fix TypeScript build errors and deploy to GitHub Pages

--- src/components/ui/Timeline.tsx
+++ src/components/ui/Timeline.tsx
@@ -64,7 +64,7 @@ export function Timeline() {
       </div>
 
       {/* Custom slider thumb styles */}
-      <style jsx>{`
+      <style>{`
         .slider-thumb::-webkit-slider-thumb {
           appearance: none;
           width: 24px;
